97a0ea30a9bae6eedc64596b00a04d091151498e
akh-ild <ildar.akhmadeyev@gmail.com>
2023-01-10 23:05:11 +0600

    fetch data + event listeners

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
   // Pick a random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  return quote;
+  console.log(quote);
+  quoteText.textContent = quote.text;
+  authorText.textContent = quote.author || 'Uknown author';
+  // Check quote length to determine styling
+  quote.text.length > 100 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote')
 }
 
 // Get quotes from API
@@ -19,6 +29,17 @@ async function getQuotes() {
     console.log(error);
   }
 }
+
+// Tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuotes();
