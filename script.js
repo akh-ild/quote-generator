9e3c614bd53574aa67481df0848fd92bb0e32701
akh-ild <ildar.akhmadeyev@gmail.com>
2023-01-11 18:08:26 +0600

    loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+  loading();
   // Pick a random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   console.log(quote);
@@ -15,10 +29,13 @@ function newQuote() {
   authorText.textContent = quote.author || 'Uknown author';
   // Check quote length to determine styling
   quote.text.length > 100 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote')
+  // Set quote, hide loader
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const res = await fetch(apiUrl);
